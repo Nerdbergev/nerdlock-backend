12ecd9e9db0ef96eb24179262ee50606a2989242
MikeTsenatek <10235033+MikeTsenatek@users.noreply.github.com>
2019-10-09 00:07:33 +0200

    minor changes

--- doorlog/models.py
+++ doorlog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import requests
 
 
 # Create your models here.
-
 class DoorStatus(models.Model):
     UNKNOWN = "unknown"
     OPEN = "open"
@@ -19,3 +21,12 @@ class DoorStatus(models.Model):
     status = models.CharField(max_length=255, choices=doorStatus, default=UNKNOWN)
     date = models.DateTimeField(default=datetime.now)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+@receiver(post_save, sender=DoorStatus)
+def my_function_post_save(sender, instance, created, **kwargs):
+    headers = {"Authorization": "Token a3cce2627e7dbe00506978ed76f807ec9cfa3e52"}
+    json = {"status": instance.status}
+    r=requests.post('http://nerdberg.de:1337/api/doorstatus/', json=json, headers=headers)
+
+    print(r.content)
